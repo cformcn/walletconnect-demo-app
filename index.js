8b55eac4eae5c9a57dc08641e3543c7e5d5aacbc
ypcai <ypcai@Ctrip.com>
2020-08-18 17:13:15 +0800

    update

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import "./shim";
 
 import { AppRegistry } from "react-native";
 import App from "./src";
-AppRegistry.registerComponent("walletconnectdemoapp", () => App);
+AppRegistry.registerComponent("fexwallet", () => App);
--- ios/fexwallettests/fexwallettests.m
+++ ios/fexwallettests/fexwallettests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface walletconnectdemoapptests : XCTestCase
+@interface fexwallettests : XCTestCase
 
 @end
 
-@implementation walletconnectdemoapptests
+@implementation fexwallettests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
